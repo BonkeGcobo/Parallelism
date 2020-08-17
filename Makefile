d1ce2f8aed20f8514689e3d2600676ed4f94c25d
Bonke Gcobo <gcbbon002@myuct.ac.za>
2020-08-17 18:35:20 +0200

    SerialProcessor is Working

---
+++ Makefile
@@ -0,0 +1,49 @@
+# Makefile for Assignment 1
+# 15/08/2020
+# Bonke Gcobo
+
+
+
+
+
+JAVAC=/usr/bin/javac
+.SUFFIXES: .java .class
+
+SRCDIR=src
+
+BINDIR=bin
+
+DOCDIR=doc
+	
+$(BINDIR)/%.class:$(SRCDIR)/%.java
+
+	$(JAVAC) -d $(BINDIR)/ -cp $(BINDIR) $<
+
+
+CLASSES=SerialProcessor.class
+	
+CLASS_FILES=$(CLASSES:%.class=$(BINDIR)/%.class)
+
+default: $(CLASS_FILES)
+
+clean:
+	@rm -f  $(BINDIR)/*.class
+	@rm -f $(SRCDIR)/*.class
+	@rm -f $(SRCDIR)/*.html
+	@rm -f $(DOCDIR)/*.csv
+	@rm -f $(DOCDIR)/*.txt
+	
+	
+
+
+removedoc:
+	rm -r ./doc
+
+doc: removedoc
+	javadoc -d $(DOCDIR) $(SRCDIR)/*.java
+		
+# running apps
+run:
+	java -cp bin SerialProcessor
+	
+
--- src/SerialProcessor.java
+++ src/SerialProcessor.java
@@ -6,6 +6,7 @@ import java.util.*;
 public class SerialProcessor{
     static int row;
     static int col;
+   
     static float arr[][];
 
     /*Reading data in the file as will the surface size*/
@@ -27,9 +28,35 @@ public class SerialProcessor{
                arr[i][j]=sc.nextFloat();
            }
        }
-       System.out.println(arr[0][1]);
-    
-       
+       System.out.println(DataProcessor(arr));
+      
+    }
+    /*Data Processing Method*/
+    public static int DataProcessor(float[][]arr){
+        int basin_counter=0;
+
+        long tick=System.currentTimeMillis();
+        for(int i=1;i<row-1;i++){
+            for(int j=1;j<col-1;j++){
+                float mid_value=arr[i][j];
+                float[] nbours = new float[]{ arr[i-1][j-1],arr[i-1][j],arr[i-1][j+1],arr[i][j-1],arr[i][j+1],
+                arr[i+1][j-1],arr[i+1][j],arr[i+1][j+1] };
+
+                int counter=0;
+                for(float k: nbours){
+                    if(k>mid_value+0.01){
+                        counter++;
+                    }
+                }
+                if(counter==8){
+                    
+                    basin_counter++;
+                }
+            }
+        }
+        long tock=System.currentTimeMillis()-tick;
+        System.out.println("Time elapsed: "+tock);
+        return basin_counter;
 
 
     }
@@ -38,6 +65,7 @@ public class SerialProcessor{
         try
         {
             Reader(args[0]);
+        
         }
         catch(FileNotFoundException ex){
             ex.printStackTrace();
